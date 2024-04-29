673ea5d28cc4402646651488be8068a0d8a545b0
Alex Moore <20783706+the0xalex@users.noreply.github.com>
2024-04-29 08:30:34 -0700

    Add colorscheme options to handle transparent bg

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -1,15 +1,29 @@
 return {
     -- Preview and select them with `:Telescope colorschemes`
     {
-        "lunarvim/lunar.nvim",
+        "scottmckendry/cyberdream.nvim",
         lazy = false,
         priority = 1000,
         config = function()
-            vim.cmd.colorscheme("lunar")
+            require("cyberdream").setup({
+                transparent = true,
+                italic_comments = true,
+                hide_fillchars = true,
+                borderless_telescope = true,
+                terminal_colors = true,
+                theme = {
+                    colors = {
+                        bgHighlight = "#0066cc",
+                    },
+                },
+            })
+            vim.cmd.colorscheme("cyberdream")
         end,
     },
+    { "lunarvim/lunar.nvim" },
     { "folke/tokyonight.nvim" },  -- The OG that the GOAT was based-on
     { "rose-pine/neovim" },       -- I like this for some languages
     { "catppuccin/nvim" },        -- other people like this.
     { "lunarvim/darkplus.nvim" }, -- for kids who recently evolved past vscode (honestly very complete)
+    { "rebelot/kanagawa.nvim" },
 }
--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -6,11 +6,7 @@ return {
         format_on_save = { timeout_ms = 500, lsp_fallback = true },
         formatters_by_ft = {
             lua = { "stylua" },
-            -- Conform can also run multiple formatters sequentially
-            -- python = { "isort", "black" },
-            --
-            -- You can use a sub-list to tell conform to run *until* a formatter
-            -- is found.
+            json = { "jq" },
             javascript = { "prettier" },
             javascriptreact = { "prettier" },
             typescript = { "prettier" },
