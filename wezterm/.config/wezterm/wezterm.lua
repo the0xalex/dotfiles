1ae5711db6ad8ccbba85aba2a81de84e251e61c9
Alex Moore <20783706+the0xalex@users.noreply.github.com>
2024-04-29 08:31:01 -0700

    Trying out transparent term

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,6 @@
 -- https://wezfurlong.org/wezterm/config/files.html
 -- Don't know lua?  read this: https://learnxinyminutes.com/docs/lua/
 local wezterm = require("wezterm")
-local keys = require("keys")
 
 local config = {}
 
@@ -9,7 +8,20 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.keys = keys
+config.keys = {
+	-- split panes
+	{
+		key = "-",
+		mods = "META",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "\\",
+		mods = "META",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+}
+
 config.window_decorations = "RESIZE"
 config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 15
@@ -21,6 +33,7 @@ config.automatically_reload_config = true
 config.default_cursor_style = "BlinkingBar"
 config.initial_cols = 120
 config.initial_rows = 70
+
 config.enable_tab_bar = true
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = false
@@ -29,11 +42,13 @@ config.show_new_tab_button_in_tab_bar = false
 
 -- config.color_scheme = "Tokyo Night"
 -- config.color_scheme = "rose-pine"
+-- config.colors = require("kanagawa").colors
 config.color_scheme = "Dark+"
 config.colors = {
 	selection_bg = "#6BB2C1",
 	selection_fg = "#292C34",
 }
+
 config.window_background_opacity = 0.80
 config.macos_window_background_blur = 20
 
