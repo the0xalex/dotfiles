f6f3f6063e3a8af896577c4b5598c4d63361346b
Alex Moore <20783706+the0xalex@users.noreply.github.com>
2024-06-01 12:44:28 -0700

    remove cyberdream from colors list

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -8,24 +8,6 @@ return {
             vim.cmd.colorscheme("lunar")
         end,
     },
-    -- {
-    --     "scottmckendry/cyberdream.nvim",
-    --      lazy = false,
-    --      priority = 1000,
-    --      config = function()
-    --         transparent = true,
-    --         italic_comments = true,
-    --         hide_fillchars = true,
-    --         borderless_telescope = true,
-    --         terminal_colors = false,
-    --         theme = {
-    --             colors = {
-    --                 bgHighlight = "#0066cc",
-    --             },
-    --         },
-    --         end,
-    --         vim.cmd.colorscheme("cyberdream")
-    --     },
     { "folke/tokyonight.nvim" },  -- The OG that the GOAT was based-on
     { "rose-pine/neovim" },       -- I like this for some languages
     { "catppuccin/nvim" },        -- other people like this.
