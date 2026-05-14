afea457bb1cec5e080fe3b1a10d0a86f38beb2df
Alex Moore <20783706+the0xalex@users.noreply.github.com>
2026-05-14 14:57:50 -0700

    Add some required treesitter parsers

--- nvim/.config/nvim/lua/autocommands.lua
+++ nvim/.config/nvim/lua/autocommands.lua
@@ -1,11 +1,10 @@
 -- Briefly highlight yanked text
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
+    group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
     pattern = "*",
     callback = function()
         vim.highlight.on_yank({ timeout = 170 })
     end,
-    group = highlight_group,
 })
 
 local is_mini_extra_loaded, mini_extra = pcall(require, "mini.extra")
